ebdcbfc2e9ad4ba017373cfbb94d72cc1c97c377
jmbutler02460 <jmbutler@bu.edu>
2022-09-14 14:53:50 -0400

    Add speed tests

--- example.cxx
+++ example.cxx
@@ -5,8 +5,8 @@
 #include <string>
 
 #include <iostream>
-
-
+#include <chrono>
+#include <random>
 
 int main(int argc, char** argv) { 
   if(argc < 2){
@@ -46,9 +46,6 @@ int main(int argc, char** argv) {
 	   (ip >> 8) & 0xFF,
 	   (ip >> 16) & 0xFF,
            (ip >> 24) & 0xFF);
-    //           (ip >> 16) & 0xFF,
-    //           (ip >> 8) & 0xFF,
-    //           ip & 0xFF);
  
     ip = SM->GetIPMCIP();    
     printf("IPMC IP = %i.%i.%i.%i\n",
@@ -67,12 +64,12 @@ int main(int argc, char** argv) {
 
     SM->unblockAXI();
     uint32_t k_mem;
-    SM->WriteRegister("K_C2C_INTF.C2C_1.DRP.ES_SDATA_MASK1",0x0);
-    k_mem = SM->ReadRegister("K_C2C_INTF.C2C_1.DRP.ES_SDATA_MASK1");
-    printf("K_C2C_INTF.C2C_1.DRP.ES_SDATA_MASK1 = 0x%08X\n",k_mem);
-    SM->WriteRegister("K_C2C_INTF.C2C_1.DRP.ES_SDATA_MASK1",0xFF);
-    k_mem = SM->ReadRegister("K_C2C_INTF.C2C_1.DRP.ES_SDATA_MASK1");
-    printf("K_C2C_INTF.C2C_1.DRP.ES_SDATA_MASK1 = 0x%08X\n",k_mem);
+    SM->WriteRegister("KINTEX_BRAM.MEM",0xAAAA5555);
+    k_mem = SM->ReadRegister("KINTEX_BRAM.MEM");
+    printf("KINTEX_BRAM.MEM = 0x%08X\n",k_mem);
+    SM->WriteRegister("KINTEX_BRAM.MEM",0x5555AAAA);
+    k_mem = SM->ReadRegister("KINTEX_BRAM.MEM");
+    printf("KINTEX_BRAM.MEM = 0x%08X\n",k_mem);
 
     // Read out the CM S/N from the DS28CM00 device
     std::cout << std::endl << "Read out the DS28CM00 device on the MPI CM" << std::endl; 
@@ -110,6 +107,87 @@ int main(int argc, char** argv) {
     SM->PowerDownCM(1,10);
     SM->GenerateStatusDisplay(1,std::cout,std::string("CM"));
     */
+
+    uint32_t loops = 1000000;
+
+    std::random_device rd;  //Will be used to obtain a seed for the random number engine
+    std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+    std::uniform_int_distribution<unsigned int> distrib(0, 0xFFFFFFFF);
+
+    auto begin = std::chrono::high_resolution_clock::now();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count();
+    double speed;
+    
+    std::cout << std::endl << "Loop test of CM write-read data integrity and speed" << std::endl << std::endl; 
+
+    for(uint32_t i = 0; i < loops; ++i) {
+      uint32_t wr = distrib(gen);
+      SM->WriteRegister("KINTEX_BRAM.MEM",wr);
+      k_mem = SM->ReadRegister("KINTEX_BRAM.MEM");
+
+      if (wr != k_mem) {
+	std::cout << "R/W error: loop " << i << ", wr = " << std::hex << wr 
+		  << ", k_mem = " << k_mem << std::endl << std::endl;
+	return -1;
+      }
+
+      if (i < 10) {
+	std::cout << "wr = " << wr << ", k_mem = " << k_mem << std::endl;
+      }
+       
+      if (i%100000 == 0 && i != 0) {
+	end = std::chrono::high_resolution_clock::now();
+	duration = std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count();
+	speed = 2.*32.*i/duration;
+	std::cout << std::dec << i << " reads done, speed = " << speed <<  " Mbps" << std::endl;
+      }
+
+    }
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count();
+
+    std::cout << std::endl << "Speed test: " << std::dec << loops << " write-reads of KINTEX_BRAM.MEM" << std::endl;
+    std::cout << duration << " us total, average : " << duration / loops << " us." << std::endl;
+
+    speed = 2.*32.*loops/duration;
+    std::cout << "Speed = " << speed << " Mbps" << std::endl;
+
+    std::cout << std::endl << "Loop test of SM write-read data integrity and speed" << std::endl << std::endl;
+
+    begin = std::chrono::high_resolution_clock::now();
+
+    for(uint32_t i = 0; i < loops; ++i) {
+      uint32_t wr = distrib(gen);
+      SM->WriteRegister("PL_MEM.SCRATCH.WORD_00",wr);
+      k_mem = SM->ReadRegister("PL_MEM.SCRATCH.WORD_00");
+
+      if (wr != k_mem) {
+	std::cout << "R/W error: loop " << i << ", wr = " << std::hex << wr
+                  << ", k_mem = " << k_mem << std::endl << std::endl;
+        return -1;
+      }
+
+      if (i < 10) {
+	std::cout << "wr = " << wr << ", k_mem = " << k_mem << std::endl;
+      }
+
+      if (i%100000 == 0 && i != 0) {
+        end = std::chrono::high_resolution_clock::now();
+        duration = std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count();
+        speed = 2.*32.*i/duration;
+	std::cout << std::dec << i << " reads done, speed = " << speed <<  " Mbps" << std::endl;
+      }
+
+    }
+    end = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(end-begin).count();
+
+    std::cout << std::endl << "Speed test: " << std::dec << loops << " write-reads of PL_MEM.SCRATCH.WORD_00" << std::endl;
+    std::cout << duration << " us total, average : " << duration / loops << " us." << std::endl;
+
+    speed = 2.*32.*loops/duration;
+    std::cout << "Speed = " << speed << " Mbps" << std::endl;
 
   }catch(BUException::exBase const & e){
     printf("Caught BUException: %s\n   Info: %s\n",e.what(),e.Description());          
